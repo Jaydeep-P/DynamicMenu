8d3e49cf382cc3eea61f5ebb14b53af7c99a6460
Jaydeep Patel <mcpe071202@gmail.com>
2022-03-22 00:34:51 +0530

    ghpages

---
+++ _next/static/Qr1BmEaluUaw1D5X39MYn/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5b16f506a9b2923f.css","static/chunks/pages/index-23c90b133b52335c.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
